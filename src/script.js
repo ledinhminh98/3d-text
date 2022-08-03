6b86fc80526cdd13b38fe6f2215cf032f9686d54
Minh <ldminh8398@gmail.com>
2022-08-03 22:41:46 +0700

    Add animation

--- src/script.js
+++ src/script.js
@@ -6,15 +6,19 @@ import "./style.css"
 
 const canvas = document.querySelector("canvas.webgl")
 const scene = new THREE.Scene()
-const textureLoader = new THREE.TextureLoader()
-const matcapTexture = textureLoader.load("textures/matcaps/3.png")
 const fontLoader = new FontLoader()
+const textureLoader = new THREE.TextureLoader()
+
+const matcapTexture = textureLoader.load("textures/matcaps/3.png")
+const cloudTexture = textureLoader.load("textures/smoke.png")
+const material = new THREE.MeshMatcapMaterial({ matcap: matcapTexture })
 
 fontLoader.load("/fonts/courgette_regular.json", (font) => {
-  // Material
-  const material = new THREE.MeshMatcapMaterial({ matcap: matcapTexture })
+  const cloudMaterial = new THREE.MeshLambertMaterial({
+    map: cloudTexture,
+    transparent: true,
+  })
 
-  // Text
   const textGeometry = new TextGeometry("Hasagi", {
     font: font,
     size: 0.5,
@@ -30,21 +34,47 @@ fontLoader.load("/fonts/courgette_regular.json", (font) => {
   const text = new THREE.Mesh(textGeometry, material)
   scene.add(text)
 
-  // Sphere
-  const sphereGeometry = new THREE.SphereGeometry(0.5, 36, 16)
-
-  for (let i = 0; i < 50; i++) {
-    const sphere = new THREE.Mesh(sphereGeometry, material)
-    sphere.position.x = (Math.random() - 0.5) * 10
-    sphere.position.y = (Math.random() - 0.5) * 10
-    sphere.position.z = (Math.random() - 0.5) * 10
-    sphere.rotation.x = Math.random() * Math.PI
-    sphere.rotation.y = Math.random() * Math.PI
-    const scale = Math.random()
-    sphere.scale.set(scale, scale, scale)
-    scene.add(sphere)
+  /**
+   * Cloud
+   */
+  const cloudGeometry = new THREE.PlaneBufferGeometry(10, 10)
+  for (let i = 0; i < 25; i++) {
+    const cloud = new THREE.Mesh(cloudGeometry, cloudMaterial)
+    cloud.position.set(
+      Math.random() * -1.5,
+      1.5 + Math.random(),
+      1 + Math.random()
+    )
+    cloud.rotation.x = 1.15
+    cloud.rotation.y = -0.25
+    cloud.rotation.z = Math.random() * 360
+    cloud.material.opacity = 0.6
+    scene.add(cloud)
   }
 })
+
+/**
+ * Rain
+ */
+const sphereGeometry = new THREE.SphereGeometry(0.03, 32, 16)
+const spheres = []
+for (let i = 0; i < 1000; i++) {
+  const sphere = new THREE.Mesh(sphereGeometry, material)
+  sphere.position.x = (Math.random() - 0.5) * 10
+  sphere.position.y = (Math.random() - 0.5) * 10
+  sphere.position.z = (Math.random() - 0.5) * 10
+  const scale = Math.random()
+  sphere.scale.set(scale, scale, scale)
+  scene.add(sphere)
+  spheres.push(sphere)
+}
+
+/**
+ * Flashlight
+ */
+const flash = new THREE.PointLight(0xffffff, 3, 0, 1.7)
+flash.position.set(2, 200, 10)
+scene.add(flash)
 
 /**
  * Sizes
@@ -55,15 +85,12 @@ const sizes = {
 }
 
 window.addEventListener("resize", () => {
-  // Update sizes
   sizes.width = window.innerWidth
   sizes.height = window.innerHeight
 
-  // Update camera
   camera.aspect = sizes.width / sizes.height
   camera.updateProjectionMatrix()
 
-  // Update renderer
   renderer.setSize(sizes.width, sizes.height)
   renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
 })
@@ -71,7 +98,6 @@ window.addEventListener("resize", () => {
 /**
  * Camera
  */
-// Base camera
 const camera = new THREE.PerspectiveCamera(
   75,
   sizes.width / sizes.height,
@@ -83,7 +109,9 @@ camera.position.y = 0.25
 camera.position.z = 2
 scene.add(camera)
 
-// Controls
+/**
+ * Controls
+ */
 const controls = new OrbitControls(camera, canvas)
 controls.enableDamping = true
 
@@ -96,21 +124,23 @@ const renderer = new THREE.WebGLRenderer({
 renderer.setSize(sizes.width, sizes.height)
 renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
 
-/**
- * Animate
- */
 const clock = new THREE.Clock()
 
 const tick = () => {
   const elapsedTime = clock.getElapsedTime()
 
-  // Update controls
+  for (let i = 0; i < 1000; i++) {
+    spheres[i].position.y = Math.sin(elapsedTime * 0.5 + i * 0.1) * 1
+  }
+
+  if (Math.random() > 0.93 || flash.power > 100) {
+    if (flash.power < 100)
+      flash.position.set(Math.random() * 2, Math.random() - 1, 0)
+    flash.power = Math.random() * 50
+  }
+
   controls.update()
-
-  // Render
   renderer.render(scene, camera)
-
-  // Call tick again on the next frame
   window.requestAnimationFrame(tick)
 }
 tick()
